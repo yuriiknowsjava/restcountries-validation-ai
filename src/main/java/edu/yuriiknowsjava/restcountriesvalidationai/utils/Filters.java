cfdf236dfa42e42d4d660b3c04e461af03420add
yuriiknowsjava <yuriiknowsjava@gmail.com>
2023-08-22 17:38:19 +0300

    UC#1 Create filter by population function

--- src/main/java/edu/yuriiknowsjava/restcountriesvalidationai/utils/Filters.java
+++ src/main/java/edu/yuriiknowsjava/restcountriesvalidationai/utils/Filters.java
@@ -1,5 +1,7 @@
 package edu.yuriiknowsjava.restcountriesvalidationai.utils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -10,13 +12,39 @@ public final class Filters {
     private Filters() {
     }
 
-    public static List<Map<String, Object>> searchByName(List<Map<String, Object>> maps, String name) {
-        var expectedName = Optional.ofNullable(name).orElse("").toLowerCase();
-        return maps.stream()
+    public static List<Map<String, Object>> searchByName(List<Map<String, Object>> countries, String name) {
+        if (name == null || name.isBlank()) {
+            return List.of();
+        }
+        var expectedName = name.toLowerCase();
+        return countries.stream()
                 .filter(Objects::nonNull)
                 .filter(map -> {
                     var actualName = map.get("name");
                     return actualName instanceof String && ((String) actualName).toLowerCase().contains(expectedName);
+                })
+                .collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @param countries - List of countries
+     * @param populationUpperBound - upper bound, in millions, for instance, 10 means 10 million.
+     * @return list of countries having less than populationUpperBound million people.
+     */
+    public static List<Map<String, Object>> searchByPopulationUpperBound(List<Map<String, Object>> countries, BigDecimal populationUpperBound) {
+        if (populationUpperBound == null) {
+            return List.of();
+        }
+        return countries.stream()
+                .filter(Objects::nonNull)
+                .filter(map -> {
+                    var population = map.get("population");
+                    if (population == null) {
+                        return false;
+                    }
+                    var actualPopulation = new BigDecimal(String.valueOf(population)).divide(new BigDecimal(1_000_000), RoundingMode.HALF_EVEN);
+                    return actualPopulation.compareTo(populationUpperBound) < 0;
                 })
                 .collect(Collectors.toList());
     }
--- src/test/java/edu/yuriiknowsjava/restcountriesvalidationai/utils/FiltersTest.java
+++ src/test/java/edu/yuriiknowsjava/restcountriesvalidationai/utils/FiltersTest.java
@@ -2,6 +2,7 @@ package edu.yuriiknowsjava.restcountriesvalidationai.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -25,5 +26,17 @@ class FiltersTest {
     private void verifySingleCountry(String expectedCountry, List<Map<String, Object>> actualResult) {
         assertEquals(1, actualResult.size());
         assertEquals(expectedCountry, actualResult.get(0).get("name"));
+    }
+
+    @Test
+    void searchByPopulationUpperBound() {
+        var eightMillion = 8_000_000;
+        List<Map<String, Object>> testData = List.of(
+                Map.of("population", 11_000_000),
+                Map.of("population", 8_000_000)
+        );
+        var result = Filters.searchByPopulationUpperBound(testData, new BigDecimal(10));
+        assertEquals(1, result.size());
+        assertEquals(eightMillion, result.get(0).get("population"));
     }
 }
