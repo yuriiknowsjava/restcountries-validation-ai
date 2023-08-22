d718246ef6521ce32f1f5da50d62f88c6bc429f2
yuriiknowsjava <yuriiknowsjava@gmail.com>
2023-08-22 13:27:51 +0300

    UC#1 Start

---
+++ src/main/java/edu/yuriiknowsjava/restcountriesvalidationai/RestcountriesValidationAiApplication.java
@@ -0,0 +1,13 @@
+package edu.yuriiknowsjava.restcountriesvalidationai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcountriesValidationAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestcountriesValidationAiApplication.class, args);
+    }
+
+}
